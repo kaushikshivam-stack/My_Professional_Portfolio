081abef36b2014e53490def267d3f79512f54e2b
Shivam Kaushik <kaushikshivam804@gmail.com>
2026-07-17 18:42:59 +0530

    fix: forward props in AppWrap and MotionWrap wrappers to fix theme-dependent profile image change

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { NavigationDots } from '../components';
 
-const AppWrap = (Component, idName, classNames) => function HOC() {
+const AppWrap = (Component, idName, classNames) => function HOC(props) {
   return (
     <div id={idName} className={`app__container ${classNames}`}>
       <div className="app__wrapper app__flex">
-        <Component />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...props} />
       </div>
       <NavigationDots active={idName} />
     </div>
--- src/wrapper/MotionWrap.js
+++ src/wrapper/MotionWrap.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-const MotionWrap = (Component, classNames) => function HOC() {
+const MotionWrap = (Component, classNames) => function HOC(props) {
   return (
     <motion.div
       whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
       transition={{ duration: 0.5 }}
       className={`${classNames} app__flex`}
     >
-      <Component />
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...props} />
     </motion.div>
   );
 };
